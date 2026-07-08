955b843f5e1901fd546c14dfcac6cb2a013fa596
sevyncreative <brandonk@sevyncreative.com>
2026-07-08 08:30:06 -0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -29,7 +29,7 @@
 */
 window.OE_CONFIG = {
   googleMapsApiKey: "AIzaSyDLvu_r04AZSs4cZvRXpZn2eFgIPWDuvVs",
-  googlePlaceId: "hIJbRWYjISYmEsR_044jIQYm8I",
+  googlePlaceId: "ChIJbRWYjISYmEsR_044jIQYm8I",
   formspreeId: "mjgqdngz",
 
   /* Used to locate the business on Google when googlePlaceId is blank,
